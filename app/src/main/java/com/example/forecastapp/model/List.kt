85a8b10e00e62439d6434694df21c11c409fb3f2
Youssef_Elfeky <youssefelfeky83@gmail.com>
2023-09-18 03:23:06 +0300

    myApp

---
+++ app/src/main/java/com/example/forecastapp/model/List.kt
@@ -0,0 +1,19 @@
+package com.example.forecastapp.model
+
+import com.example.example.forecastapp.*
+import com.google.gson.annotations.SerializedName
+
+
+data class List (
+
+  @SerializedName("dt"         ) var dt         : Int?               = null,
+  @SerializedName("main"       ) var main       : Main?              = Main(),
+  @SerializedName("weather"    ) var weather    : ArrayList<Weather> = arrayListOf(),
+  @SerializedName("clouds"     ) var clouds     : Clouds?            = Clouds(),
+  @SerializedName("wind"       ) var wind       : Wind?              = Wind(),
+  @SerializedName("visibility" ) var visibility : Int?               = null,
+  @SerializedName("pop"        ) var pop        : Int?               = null,
+  @SerializedName("sys"        ) var sys        : Sys?               = Sys(),
+  @SerializedName("dt_txt"     ) var dtTxt      : String?            = null
+
+)
