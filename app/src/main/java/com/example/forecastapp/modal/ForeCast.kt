6c95937bc8e6f2975cd4a1f1fd62d7caa943d19b
Youssef_Elfeky <youssefelfeky83@gmail.com>
2023-09-19 06:17:10 +0300

    myApp

--- app/src/main/java/com/example/forecastapp/modal/ForeCast.kt
+++ app/src/main/java/com/example/forecastapp/modal/ForeCast.kt
@@ -9,7 +9,7 @@ data class ForeCast (
   @SerializedName("cod"     ) var cod     : String?         = null,
   @SerializedName("message" ) var message : Int?            = null,
   @SerializedName("cnt"     ) var cnt     : Int?            = null,
-  @SerializedName("list"    ) var list    : ArrayList<WeatherList> = arrayListOf(),
+  @SerializedName("list"    ) var weatherList    : ArrayList<WeatherList> = arrayListOf(),
   @SerializedName("city"    ) var city    : City?           = City()
 
 )
--- app/src/main/java/com/example/forecastapp/mvvm/WeatherVm.kt
+++ app/src/main/java/com/example/forecastapp/mvvm/WeatherVm.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.forecastapp.MyApplication
 import com.example.forecastapp.SharedPrefs
 import com.example.forecastapp.modal.WeatherList
+import com.example.forecastapp.service.RetrofitInstance
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.time.LocalDateTime
@@ -33,6 +34,35 @@ class WeatherVm:ViewModel() {
         val lat = sharedPrefs.getValue("lat").toString()
         val lon = sharedPrefs.getValue("lon").toString()
 
+        val call = if(city!=null)
+        {
+            RetrofitInstance.api.getWeatherByCity(city)
+        }
+        else{
+            RetrofitInstance.api.getWeatherByCity(lat,lon)
+        }
+
+        val response = call.execute()
+
+        if(response.isSuccessful)
+        {
+            val weatherList = response.body()?.weatherList
+            cityName.postValue(response.body()?.city!!.name )
+
+            val presentDate = currentDatePattern
+
+            weatherList?.forEach{weather ->
+                if(weather.dtTxt!!.split("\\s".toRegex()).contains(presentDate))
+                {
+                    todayWeatherList.add(weather)
+                }
+            }
+        }
+
+        val closeWeather = findCloseWeather(todayWeatherList)
+        closetorexactlysameweatherdata.postValue(closeWeather)
+
+        todayWeatherLiveData.postValue(todayWeatherList)
     }
 
 }
