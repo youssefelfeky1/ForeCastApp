d5e50f30327acd4f22b24ef3ee55589399411465
Youssef_Elfeky <youssefelfeky83@gmail.com>
2023-09-20 09:22:01 +0300

    myApp

--- app/src/main/java/com/example/forecastapp/Utils.kt
+++ app/src/main/java/com/example/forecastapp/Utils.kt
@@ -3,7 +3,7 @@ package com.example.forecastapp
 class Utils {
     companion object{
         var BASE_URL : String = "https://api.openweathermap.org/data/2.5/"
-        var API_KEY:String ="1fafd3e34a12b87346fb15bd0eed1e18"
+        var API_KEY:String ="insert your own key"
         const val PERMISSION_REQUEST_CODE = 123
     }
 }
